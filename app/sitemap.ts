e4d40adccbadcd55a458560f1405957b7151afb4
Priyesh Khamar <priyeshkhamar76@gmail.com>
2026-07-23 18:30:49 +0530

    changes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 import { caseStudies } from "@/lib/case-studies";
 import { site } from "@/lib/data";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const lastModified = new Date();
   return [
